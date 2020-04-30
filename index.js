e34f178fcbb10fcf23aa6e94ab3b949f2031ff26
Juggernaut9 <xbhavyatomar@gmail.com>
2020-04-30 16:02:15 +0530

    added table sorted by confirmed total cases

--- index.js
+++ index.js
@@ -21,4 +21,31 @@ function getCovidStats(){
   .catch(function(err){
     console.log(err);
   })
+
+  fetch('https://disease.sh/v2/countries?sort=cases')
+  .then(function(resp){
+    if (!resp.ok) throw new Error(`all countries Status code Error: ${resp.status}`);
+    return resp.json();
+  })
+  .then(function(data){
+    for (let i=0; i<10; i++){
+      let country = data[i].country;
+      let cases = data[i].cases;
+      let recovered = data[i].recovered;
+      let deaths = data[i].deaths;
+      console.log(country,cases,recovered,deaths);
+
+      const tr = document.createElement('tr')
+      tr.innerHTML = `<td>${country}</td><td>${cases}</td><td>${recovered}</td><td>${deaths}</td>`
+      i%2===0 ? tr.classList.add('even-table') : tr.classList.add('odd-table');
+      const tbody = document.querySelector('tbody');
+      tbody.appendChild(tr);
+    }
+  })
+  .catch(function(err){
+    console.log(err);
+  })
 }
+
+//lighter blue : e0fcff
+//light blue: 90f2ff
